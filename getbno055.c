91779734da65649464538a248e8619a433e75685
Brian Holt <bdholt1@gmail.com>
2021-06-03 08:36:39 +0000

    fixed formatting to 8 spaces

--- getbno055.c
+++ getbno055.c
@@ -755,7 +755,7 @@ int main(int argc, char *argv[]) {
        * EUL 66.06 -3.00 -15.56 (EUL H R P in Degrees)               *
        * ----------------------------------------------------------- */
       while(1){
-	clock_t t;
+        clock_t t;
         t = clock();
 
         res = get_eul(&bnod);
@@ -785,7 +785,7 @@ int main(int argc, char *argv[]) {
          fclose(html);
         }
 
-	t = clock() - t;
+        t = clock() - t;
         double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
         printf("Sensor reading took %f seconds \n", time_taken);
       }
